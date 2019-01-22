3a57c847e8b74c91fa0268b0bdda555518953804
tiagostutz <tiagostutz@gmail.com>
2019-01-22 13:50:17 -0200

    tested successfully with 2 spans

--- jaeger-remote/main.go
+++ jaeger-remote/main.go
@@ -7,14 +7,15 @@ import (
 	"io/ioutil"
 	"net/http"
 
+	"github.com/opentracing/opentracing-go"
+
 	"github.com/gorilla/mux"
-	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	jaeger "github.com/uber/jaeger-client-go"
 	"github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-client-go/log"
-	"github.com/uber/jaeger-lib/metrics"
+	jaegerProm "github.com/uber/jaeger-lib/metrics/prometheus"
 	"go.uber.org/zap"
 )
 
@@ -43,6 +44,8 @@ var listenIP string
 var listenPort int
 
 var jaegerLocalAgentHostPort, jaegerServiceName string
+var traceConfig config.Configuration
+var metricsFactory *jaegerProm.Factory
 
 func main() {
 	defer logger.Sync() // flushes buffer, if any
@@ -56,31 +59,21 @@ func main() {
 	flag.Parse()
 
 	sugar.Infof("Initialiazing Jaeger....")
-	sugar.Infof("Service Name: %s", jaegerServiceName)
+	sugar.Infof("Global Service Name: %s", jaegerServiceName)
 	sugar.Infof("LocalAgentHostPort: %s", jaegerLocalAgentHostPort)
 
-	if jaegerServiceName != "" {
-
-		sugar.Infof("Jaeger Service Name provided. Setting up a Global Tracer for service '%s'", jaegerServiceName)
-		cfg := &config.Configuration{
-			ServiceName: jaegerServiceName,
-			Sampler: &config.SamplerConfig{
-				Type:  jaeger.SamplerTypeConst,
-				Param: 1,
-			},
-			Reporter: &config.ReporterConfig{
-				LogSpans:           true,
-				LocalAgentHostPort: jaegerLocalAgentHostPort,
-			},
-		}
-		tracer, closer, err := cfg.NewTracer(config.Logger(jaegerlog.StdLogger), config.Metrics(metrics.NullFactory))
-		if err != nil {
-			panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
-		}
-		opentracing.SetGlobalTracer(tracer)
-		defer closer.Close()
-
+	traceConfig = config.Configuration{
+		Sampler: &config.SamplerConfig{
+			Type:  jaeger.SamplerTypeConst,
+			Param: 1,
+		},
+		Reporter: &config.ReporterConfig{
+			LogSpans:           true,
+			LocalAgentHostPort: jaegerLocalAgentHostPort,
+		},
 	}
+
+	metricsFactory = jaegerProm.New()
 
 	prometheus.MustRegister(endpointRequest)
 	prometheus.MustRegister(traceRecord)
@@ -88,6 +81,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc(traceEndpointName, handleCreateTrace).Methods("POST")
 	router.Handle("/metrics", promhttp.Handler())
+
 	listen := fmt.Sprintf("%s:%d", listenIP, listenPort)
 	sugar.Infof("Listening at %s", listen)
 	err2 := http.ListenAndServe(listen, router)
@@ -99,9 +93,6 @@ func main() {
 
 func handleCreateTrace(w http.ResponseWriter, r *http.Request) {
 	sugar.Debugf("Parsing input json to map")
-
-	originStr := r.Header.Get("Trace-Origin")
-	traceRecord.WithLabelValues(originStr).Inc()
 
 	bodyBytes, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -123,16 +114,30 @@ func handleCreateTrace(w http.ResponseWriter, r *http.Request) {
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	var context = ""
+	var context map[string]interface{}
 	if jsonInput["context"] != nil {
-		context = jsonInput["context"].(string)
+		context = jsonInput["context"].(map[string]interface{})
 	}
 	var tags = make(map[string]interface{})
-	if jsonInput["context"] != nil {
+	if jsonInput["tags"] != nil {
 		tags = jsonInput["tags"].(map[string]interface{})
 	}
-	output := Trace(jsonInput["name"].(string), context, tags)
+
+	var serviceName = jaegerServiceName
+	if serviceName == "" || jsonInput["serviceName"].(string) != "" { //if the serviceName is provided, it will override the global one
+		serviceName = jsonInput["serviceName"].(string)
+	}
+
+	// count traces by service name
+	traceRecord.WithLabelValues(serviceName).Inc()
+
+	contextTrace := make(map[string]string)
+	for key, value := range context {
+		contextTrace[key] = value.(string)
+	}
+	output := Trace(serviceName, jsonInput["spanName"].(string), contextTrace, tags)
 	outBytes, err := json.Marshal(output)
+
 	if err != nil {
 		sugar.Warnf("Error converting tracing data to JSON. Error details: %s", err)
 		endpointRequest.WithLabelValues("500", traceEndpointName).Inc()
@@ -147,16 +152,37 @@ func handleCreateTrace(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Error writing response. Error: "+err.Error(), 500)
 		return
 	}
+
+	endpointRequest.WithLabelValues("200", traceEndpointName).Inc()
 }
 
-// Trace record the trace with the values received
-func Trace(name string, context string, tags map[string]interface{}) map[string]interface{} {
-	output := make(map[string]interface{})
-	tracer := opentracing.GlobalTracer()
+// Trace record the trace with the values received and returns its context
+func Trace(serviceName string, spanName string, context map[string]string, tags map[string]interface{}) map[string]string {
+	output := make(map[string]string)
 
-	span := tracer.StartSpan(name)
-	println(name)
+	tracer, closer, err := traceConfig.New(serviceName, config.Logger(jaegerlog.StdLogger), config.Metrics(metricsFactory))
+	if err != nil {
+		panic(fmt.Sprintf("ERROR: cannot init Jaeger: %v\n", err))
+	}
+	defer closer.Close()
+
+	var span opentracing.Span
+	if context != nil {
+		spanCtx, err := tracer.Extract(opentracing.TextMap, opentracing.TextMapCarrier(context))
+		if err == nil {
+			span = tracer.StartSpan(spanName, opentracing.ChildOf(spanCtx))
+		} else { // if there's a error extracting, trace, but put a tag pointing the error
+			span = tracer.StartSpan(spanName)
+			span.SetTag("trace-fallback", "error-retrieving-the-context")
+		}
+	} else {
+		span = tracer.StartSpan(spanName)
+	}
+	println(spanName)
 	span.Finish()
+
+	// return the Context in the response so the client can forward it to keep the trace chain
+	span.Tracer().Inject(span.Context(), opentracing.TextMap, opentracing.TextMapCarrier(output))
 
 	return output
 }
